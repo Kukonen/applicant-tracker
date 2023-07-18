0bd5fb530bd3de54a38808daeb386432d3a8ba4e
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2023-07-18 18:06:21 +0300

    first backend commit

---
+++ backend/applicant-tracker/src/main/java/org/applicant/tracker/dao/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package org.applicant.tracker.dao.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource hikariPostgresDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+}
