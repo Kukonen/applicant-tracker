219332f20991f6d454405357627466ba1cde9c0e
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2023-07-20 13:47:33 +0300

    База данных создана 2/2
    Доделал сущность Candidate, все запросы к БД и тесты

--- backend/applicant-tracker/src/main/java/org/applicant/tracker/dao/service/CandidateService.java
+++ backend/applicant-tracker/src/main/java/org/applicant/tracker/dao/service/CandidateService.java
@@ -2,6 +2,7 @@ package org.applicant.tracker.dao.service;
 
 import org.applicant.tracker.dao.dto.Candidate;
 import org.applicant.tracker.dao.mapper.ICandidateMapper;
+import org.applicant.tracker.exceptions.DatabaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,16 @@ public class CandidateService extends AbstractDatabaseService<ICandidateMapper, 
 
     List<Candidate> getByProgramId(Long programId) {
         return mapper.getByProgramId(programId);
+    }
+
+    Candidate getBySnils(String snils) throws DatabaseException {
+        Candidate candidate = mapper.getBySnils(snils);
+
+        if (candidate == null) {
+            throw new DatabaseException("Абитуриент с таким СНИЛС не был обнаружен в таблице!");
+        }
+
+        return candidate;
     }
 
 }
